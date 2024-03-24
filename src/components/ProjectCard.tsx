0334c593716043346a9bf4b00036dd901a92044a
Juan Esteban Caicedo Valencia <imjuan3772@gmail.com>
2024-03-24 14:31:01 -0500

    add slider functionality

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,10 +7,12 @@ type ProjectCardProps = {
 
 function ProjectCard({ title, description, image }: ProjectCardProps) {
   return (
-    <div className="w-[450px] m-5 rounded-lg shadow-lg border-2 border-gray bg-black flex flex-col justify-center items-center text-white transition-all duration-200 transform hover:scale-105 z-10">
+    <div className="min-w-[450px] m-5 rounded-lg shadow-lg border-2 border-gray bg-black flex flex-col justify-center items-center text-white transition-all duration-200 transform hover:scale-105 z-10 whitespace-nowrap">
       <img src={image} alt="" className='p-2 w-full h-[250px]'/>
       <h1 className='border-b-2 border-blue-500 text-4xl font-light font-rowdies py-1'>{title}</h1>
-        <p className='text-lg text-center py-3 px-4 font-mono'>{description}</p>
+      <div className="max-w-[450px] overflow-hidden whitespace-normal">
+  <p className="text-lg text-center py-3 px-4 font-mono">{description}</p>
+</div>
         <button className="bg-gradient-to-r from-blue-500 to-red-600 text-white text-2xl font-russo rounded-3xl px-6 py-4 m-4">View Project</button>
     </div>
   )
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,21 +1,43 @@
+import { useRef } from 'react'
 import ProjectCard from "../components/ProjectCard"
 import coffeShop from '../images/coffeshop-project.jpg'
 import fastFood from '../images/fastfood-project.jpg'
 import supermarket from '../images/supermarket-project.jpg'
 
 function Projects() {
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = ( direction: 'left' | 'right' ) => {
+    if( direction === 'left' ) {
+      scrollContainerRef.current?.scrollBy({
+        left: -490,
+        behavior: 'smooth'
+      })
+    }
+    if( direction === 'right' ) {
+      scrollContainerRef.current?.scrollBy({
+        left: 490,
+        behavior: 'smooth'
+      })
+    }
+  }
+
   return (
     <div className="w-full min-h-[100vh] bg-black text-white text-center flex flex-col justify-center items-center" id="projects">
       <h1 className="text-center text-5xl font-bold py-2 mt-[10vh]">Projects</h1>
-      <div className="flex flex-wrap justify-center items-center w-full">
-        <ProjectCard title="Coffe Shop Website" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={coffeShop}/>
+      <button onClick={() => handleScroll('left')}>Prev</button>
+      <div ref={scrollContainerRef} className="overflow-hidden whitespace-nowrap flex w-[80%] ">
+        <ProjectCard title="Coffe Shop Website" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={coffeShop} />
         <ProjectCard title="Fast Food Website" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={fastFood}/>
-        <ProjectCard title="SuperMarket E-commerce" description="
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={supermarket} />
+        <ProjectCard title="SuperMarket E-commerce" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={supermarket} />
+        <ProjectCard title="SuperMarket E-commerce" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={supermarket} />
+        <ProjectCard title="SuperMarket E-commerce" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={supermarket} />
       </div>
+      <button onClick={() => handleScroll('right')}>Next</button>
     </div>
   )
 }
 
-export default Projects
+export default Projects  
 
