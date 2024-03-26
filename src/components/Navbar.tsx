f1cd6aaf0fd8bec58774622e64dea8874574338f
Juan Esteban Caicedo Valencia <imjuan3772@gmail.com>
2024-03-26 10:10:26 -0500

    fix responsive design bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,49 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Logo from '../images/logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
 
 function Navbar() {
+  const [ isOpen, setIsOpen ] = useState(false)
   const scrollTo = (id: string) => {
     const element = document.getElementById(id);
     console.log(element)
     element?.scrollIntoView({ behavior: 'smooth' });
   }
   return (
-    <div className="w-full bg-black flex items-center justify-center text-white text-2xl h-[10vh] fixed top-0 z-50">
-      <ul className='h-full flex items-center  absolute left-0'>
-
-      <img src={Logo} alt="" className='w-full h-full object-contain select-none cursor-pointer' onClick={() => scrollTo('about')} />
-      <h1 className="text-4xl font-extrabold font-mono select-none cursor-pointer" onClick={() => scrollTo('about')}>JuanCoding</h1>
+    <div className="w-full flex items-center justify-center text-white text-2xl min-h-[10vh] fixed top-0 z-50 bg-black">
+      <ul className='h-full flex items-center absolute left-0 '>
+      <img src={Logo} alt="" className='w-full h-[70px] object-contain select-none cursor-pointer' onClick={() => scrollTo('about')} />
+      <h1 className="text-2xl font-extrabold font-mono select-none cursor-pointer" onClick={() => scrollTo('about')}>JuanCoding</h1>
       </ul>
-      <ul className="flex w-1/2 justify-around font-normal font-sans text-4xl select-none cursor-pointer">
+
+      <div>
+      <button className="focus:outline-none lg:hidden flex absolute right-0 top-0 p-2 m-5 " onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <CloseIcon fontSize='large'/> : <MenuIcon fontSize='large'/>}
+      </button>
+      </div>
+      <ul className="hidden w-1/2 justify-around font-normal font-sans text-4xl select-none cursor-pointer lg:flex">
       <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('projects')}>Projects</button>
       <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('technologies')}>Technologies</button>
       <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('about')}>About Me</button>
       </ul>
-      <ul className="flex justify-around text-xl w-[210px] absolute right-0">
-        <li>
-            <LinkedInIcon style={{ fontSize: 60}} className='text-[#0a66c2]'/>
-        </li>
-        <li>
-            <GitHubIcon style={{ fontSize: 50 }}/>
-        </li>
+      {isOpen && (
+      <ul className="flex w-full justify-around items-start font-normal flex-col font-sans text-xl select-none cursor-pointer translate-y-[14vh] absolute left-0 bg-black py-5">
+      <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('projects')}>Projects</button>
+      <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('technologies')}>Technologies</button>
+      <button className="focus:outline-none border-b-4 border-transparent focus:border-blue-500" onClick={() => scrollTo('about')}>About Me</button>
+      <li>
+        <a href="http://" className='mx-2'>
+          <GitHubIcon />
+        </a>
+        <a href="http://" className='mx-2'>
+          <LinkedInIcon />  
+        </a> 
+      </li>
       </ul>
+      )}
     </div>
   )
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ type ProjectCardProps = {
 
     function ProjectCard({ title, description, image }: ProjectCardProps) {
       return (
-        <div className="min-w-full md:min-w-[45%] lg:min-w-[350px] xl:min-w-[400px] md:m-[16px] rounded-lg shadow-lg border-2 border-gray bg-black flex flex-col justify-center items-center text-white transition-all duration-200 transform hover:scale-105 z-10 whitespace-normal">
+        <div className="min-w-full md:min-w-[45%] lg:min-w-[350px] xl:min-w-[400px] md:m-[16px] rounded-lg shadow-lg border-2 border-gray bg-black flex flex-col justify-center items-center text-white transition-all duration-200 transform md:hover:scale-105 z-10 whitespace-normal">
           <img src={image} alt="" className='p-2 w-full h-[250px]'/>
           <h1 className='border-b-2 border-blue-500 text-xl md:text-3xl font-light font-rowdies'>{title}</h1>
           <div className="max-w-[450px] overflow-hidden whitespace-normal">
