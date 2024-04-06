8316613c7ffeb925a8b93f80e506bce9d4f87add
Juan Esteban Caicedo Valencia <imjuan3772@gmail.com>
2024-04-05 20:04:13 -0500

    Update ProjectCard and Contact components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,25 +5,25 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function LetsWork() {
   return (
-    <div className='w-full min-h-[1024px] max-h-[1024px] flex flex-col items-center justify-center bg-black text-white' id="contact">
+    <div className='w-full min-h-[1024px] flex flex-col items-center justify-center bg-black text-white' id="contact">
       <h1 className='text-4xl md:text-6xl font-bold text-center mt-28'>Contact Me</h1>
       <p className='text-2xl font-mono my-5 text-center'>Vamos a trabajar juntos!</p>
     <div className='flex w-[90%] lg:w-3/4 flex-col md:flex-row justify-center my-8'>
       <ul className='w-full md:w-1/2'>
         <h1 className='text-purple-700 text-3xl md:text-4xl font-bold font-rowdies'>Informacion de Contacto</h1>
-        <a href='https://wa.me/+573107642808?text=mensaje' target='_blank' className='flex text-green-500 text-xl items-center justify-start font-mono py-3'>
+        <a href='https://wa.me/+573107642808?text=mensaje' target='_blank' className='flex text-green-500 hover:text-green-600 text-xl items-center justify-start font-mono py-3 w-1/2'>
           <WhatsAppIcon fontSize='large'/>
           WhatsApp
         </a>
-        <a href='#' target='_blank' className='flex text-white text-xl items-center justify-start font-mono py-3'>
+        <a href='#' target='_blank' className='flex text-white hover:text-gray-300 text-xl items-center justify-start font-mono py-3 w-1/2'>
           <EmailIcon fontSize='large'/>
           imjuan3772@gmail.com
         </a>
-        <a href='https://www.linkedin.com/in/juan-esteban-caicedo-valencia-a07551264/' target='_blank' className='flex text-xl justify-start items-center text-blue-500 font-mono py-3'>
+        <a href='https://www.linkedin.com/in/juan-esteban-caicedo-valencia-a07551264/' target='_blank' className='flex text-xl justify-start items-center text-blue-500 hover:text-blue-600 font-mono py-3 w-1/2'>
           <LinkedInIcon fontSize='large'/>
           LinkedIn
         </a>
-        <a href='https://github.com/Jayz1x' target='_blank' className='flex text-xl justify-start items-center text-white font-mono py-3'>
+        <a href='https://github.com/Jayz1x' target='_blank' className='flex text-xl justify-start items-center text-white hover:text-gray-300 font-mono py-3 w-1/2'>
           <GitHubIcon fontSize='large'/>
           GitHub
         </a>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -38,11 +38,40 @@ function Projects() {
           <ChevronLeftIcon fontSize='large'/>
         </button>
         <div ref={scrollContainerRef} className="overflow-hidden whitespace-nowrap w-full lg:w-[1200px] flex items-center h-[700px]">
-        <ProjectCard title="Coffe Shop Website" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={coffeShop} />
-        <ProjectCard title="Fast Food Website" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={fastFood} isMiddleIndex/>
-        <ProjectCard title="Panda E-commerce" description="desarrollé junto a un equipo de trabajo una página web completa para un supermercado, utilizando React, TypeScript y Tailwind CSS en el frontend, y Node.js, Express.js y MongoDB en el backend. La aplicación ofrece una experiencia de compra en línea intuitiva y satisfactoria para los usuarios." image={supermarket} />
-        <ProjectCard title="Amazon FullStack Clon" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas? asdfasdfasdf asdfasdfasdf asdfasdfasdf asdfasdfasdf " image={Amazon} isMiddleIndex/>
-        <ProjectCard title="Backend E-commerce" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" image={Backend} />
+        <ProjectCard 
+        title="Coffe Shop Website" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" 
+        image={coffeShop} 
+        URL=''/>
+
+        <ProjectCard 
+        title="Fast Food Website" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" 
+        image={fastFood} 
+        isMiddleIndex
+        URL='https://jayz1x.github.io/FAST-FOOD-WEBSITE/' 
+        />
+
+        <ProjectCard
+        title="Panda E-commerce" 
+        description="desarrollé junto a un equipo de trabajo una página web completa para un supermercado, utilizando React, TypeScript y Tailwind CSS en el frontend, y Node.js, Express.js y MongoDB en el backend. La aplicación ofrece una experiencia de compra en línea intuitiva y satisfactoria para los usuarios." 
+        image={supermarket} 
+        URL='https://ecommerce-frontend-alpha-eight.vercel.app/'
+        />
+
+        <ProjectCard 
+        title="Amazon FullStack Clon" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas? asdfasdfasdf asdfasdfasdf asdfasdfasdf asdfasdfasdf " 
+        image={Amazon} 
+        isMiddleIndex
+        URL='https://clone-7b7b7.web.app/'
+        />
+
+        <ProjectCard 
+        title="Backend E-commerce" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam reiciendis non molestiae neque expedita, voluptatem nobis repellat quasi iure molestias est asperiores. Ab similique sunt ex, sed laboriosam illum voluptas?" 
+        image={Backend}
+        URL='https://ecommerce-backend-eta-two.vercel.app/api/products' />
         </div>
         <button onClick={() => handleScroll('right')} className='bg-purple-600 rounded-full p-4 flex translate-x-[-50%] z-20'>
           <ChevronRightIcon fontSize='large'/>
